44d5d4aba0c364f61226a20dfa9158677d511d6c
Ruchir Kafle <Ruchir@webdev11.local>
2024-06-03 11:02:16 -0400

    message

--- app.js
+++ app.js
@@ -15,21 +15,54 @@ function createTodo(desc, priority=false) {
     data.push(
         {
             todo: desc,
+            priority,
             done: false,
-            priority
         }
     );
-
-    for (task of data) {
-        
-    };
 
     return true;
 }
 
+function render() {
+    let todoList = document.querySelector(".todo-list")
+    
+    let counter = 0;
+    todoList.innerHTML = ``;
+    for (task of data) {
+        let priorityDisplay = ``;
+        if (task.priority) {
+            priorityDisplay = ` *`
+        }
+        
+        todoList.innerHTML += `<li>
+        <input type="checkbox" id="check-${counter}">
+        <label for="check-${counter}">${task.todo}${priorityDisplay}</label>
+        </li>`;
+
+        if (task.done) {
+            
+        }
+        
+        counter += 1
+    }
+
+    let todos = todoList.querySelectorAll('input[type="checkbox"]');
+    for (checkbox of todos) {
+        checkbox.addEventListener("change", handleCheck);
+    }
+}
+
+function handleCheck(evt) {
+    evt.currentTarget.parentElement.querySelector("label").classList.toggle("checked");
+    let id = evt.currentTarget.id.split('-');
+    data[parseInt(id)].done = true;
+}
+
 function handleAdd() {
     let desc = document.querySelector("#todo");
-    createTodo(desc.value);
+    let priorityBox = document.querySelector("#priority")
+    createTodo(desc.value, priorityBox.checked);
+    render()
 }
 
 addButton.addEventListener("click", handleAdd);
